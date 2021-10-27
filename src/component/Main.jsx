835d679843f8ab8d4a5b1514d718346b18b9a5eb
master <ytooktoo2@nate.com>
2021-10-27 22:36:46 +0900

    푸터 추가

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,18 +1,21 @@
-import React, { Fragment,useEffect } from "react";
-import { Container } from "@mui/material";
+import React, { Fragment, useEffect } from "react";
+import { Container, Typography } from "@mui/material";
 import Developer from "./Developer";
 import DevelopStyle from "./DevelopStyle";
 import PortFolio from "./PortFolio";
 export default function Main() {
-    useEffect(()=>{
-        
-    },[]);
+    useEffect(() => {
+
+    }, []);
     return (
         <Fragment>
             <Container maxWidth="sm">
                 <Developer />
-                <DevelopStyle/>
-                <PortFolio/>
+                <DevelopStyle />
+                <PortFolio />
+                <footer style={{ bottom: 0, position: "sticky", backgroundColor: "#DDDDDD" }}>
+                    <Typography variant="body1" color="textSecondary" align="center" style={{ fontWeight: "bold" }}>Copyright © 이철웅 ytooktoo2@nate.com</Typography>
+                </footer>
             </Container>
         </Fragment>
     );
