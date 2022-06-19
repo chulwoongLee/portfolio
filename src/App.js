04a14d2fd945d6779c49bd00777a760683a6aba5
master <ytooktoo2@nate.com>
2022-06-19 22:22:01 +0900

    최신화

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import Main from "./component/Main";
+import { Snackbar, Typography } from "@mui/material";
+import { CheckCircleOutline } from "@mui/icons-material";
+import { useState } from "react";
 function App() {
+  const [finishPage, setFinishPage] = useState(true);
   return (
     <div className="App">
-      <Main/>
+      <Main />
+      <Snackbar
+        open={finishPage}
+        autoHideDuration={5000}
+        onClose={() => {
+          console.log("종료");
+          setFinishPage(false);
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: "#EDF7ED",
+            display: "flex",
+            width: "100%",
+          }}
+        >
+          <CheckCircleOutline
+            style={{ color: "#80C683", fontWeight: "bold" }}
+            fontSize="large"
+          />
+          <Typography
+            style={{
+              color: "#1E595F",
+              fontWeight: "bold",
+              alignSelf: "center",
+            }}
+            variant="body1"
+          >
+            oracle-cloud 이슈로 몇몇 예제페이지 안됩니다.
+          </Typography>
+        </div>
+      </Snackbar>
     </div>
   );
 }
--- src/component/Developer.jsx
+++ src/component/Developer.jsx
@@ -2,66 +2,78 @@ import React, { Fragment, useEffect, useState } from "react";
 import { Card, Typography, Grid } from "@mui/material";
 import StickHeader from "./StickHeader";
 export default function Developer() {
-    return (
-        <Fragment>
-            <StickHeader title="개발자 스킬" />
-            <div style={{ flex: 1, backgroundColor:"#92A9DE",/*background: "linear-gradient(to right top, #00bed8, #00d4cb, #60e6ae, #acf38b, #f9f871)"*/ }}>
-                {/*
+  return (
+    <Fragment>
+      <StickHeader title="개발자 스킬" />
+      <div
+        style={{
+          flex: 1,
+          backgroundColor:
+            "#92A9DE" /*background: "linear-gradient(to right top, #00bed8, #00d4cb, #60e6ae, #acf38b, #f9f871)"*/,
+        }}
+      >
+        {/*
                 linear-gradient(to right top, #1976d2, #7894dd, #aeb5e8, #d9d9f3, #ffffff)
             linear-gradient(to right top, #00bed8, #00d4cb, #60e6ae, #acf38b, #f9f871);
                 */}
 
-                <br />
-                <div style={{ margin: 10 }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={6}>
-                            <Card style={{marginBottom:10}}>
-                                <ul>
-                                    <Typography style={{ fontWeight: "bold" }} variant="h6">WEB</Typography>
-                                    <li>REACT
-                                        <ul>
-                                            <li>렌더링 퍼포먼스</li>
-                                            <li>rest-api-렌더링</li>
-                                        </ul>
-                                    </li>
-                                    <li>JAVASCRIPT</li>
-                                    <ul>
-                                        <li>ES6문법 사용</li>
-                                        <li>Redux</li>
-                                        <li>Redux-saga</li>
-                                    </ul>
-                                    <li>JSP</li>
-                                </ul>
-                            </Card>
-                            <Card style={{marginBottom:10}}>
-                                <ul><Typography style={{ fontWeight: "bold" }} variant="h6">DB</Typography>
-                                    <li>ORACLE</li>
-                                </ul>
-                            </Card>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Card style={{marginBottom:10}}>
-                                <ul><Typography style={{ fontWeight: "bold" }} variant="h6">APP</Typography>
-                                    <li>REACT-NATIVE(EXPO)
-                                        <ul>
-                                            <li>noti-push</li>
-                                            <li>google-map</li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </Card>
-                            <Card style={{marginBottom:10}}>
-                                <ul><Typography style={{ fontWeight: "bold" }} variant="h6">SERVER</Typography>
-                                    <li>NODE.JS</li>
-                                    <li>SPRING-BOOT</li>
-                                    <li>GRAPHQL</li>
-                                </ul>
-                            </Card>
-                        </Grid>
-                    </Grid>
-
-                </div>
-            </div>
-        </Fragment>
-    );
+        <br />
+        <div style={{ margin: 10 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Card style={{ marginBottom: 10 }}>
+                <ul>
+                  <Typography style={{ fontWeight: "bold" }} variant="h6">
+                    WEB
+                  </Typography>
+                  <li>
+                    REACT
+                    <ul>
+                      <li>nextjs</li>
+                    </ul>
+                  </li>
+                  <li>JAVASCRIPT</li>
+                  <li>JSP</li>
+                </ul>
+              </Card>
+              <Card style={{ marginBottom: 10 }}>
+                <ul>
+                  <Typography style={{ fontWeight: "bold" }} variant="h6">
+                    DB
+                  </Typography>
+                  <li>ORACLE</li>
+                </ul>
+              </Card>
+            </Grid>
+            <Grid item xs={6}>
+              <Card style={{ marginBottom: 10 }}>
+                <ul>
+                  <Typography style={{ fontWeight: "bold" }} variant="h6">
+                    APP
+                  </Typography>
+                  <li>
+                    REACT-NATIVE(EXPO)
+                    <ul>
+                      <li>noti-push</li>
+                      <li>google-map</li>
+                    </ul>
+                  </li>
+                </ul>
+              </Card>
+              <Card style={{ marginBottom: 10 }}>
+                <ul>
+                  <Typography style={{ fontWeight: "bold" }} variant="h6">
+                    SERVER
+                  </Typography>
+                  <li>NODE.JS</li>
+                  <li>SPRING-BOOT</li>
+                  <li>GRAPHQL</li>
+                </ul>
+              </Card>
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
